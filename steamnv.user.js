d3ececd86f5725e8a40b656b37db3476e48c62f3
Beariddle <82675223+Beariddle@users.noreply.github.com>
2022-06-03 22:56:27 +0200

    Update steamnv.user.js
    [x] Cleaned up the code.
    [x] Simplified manually inserting of date of birth values.
    [x] Updated comments where applicable.

--- steamnv.user.js
+++ steamnv.user.js
@@ -1,50 +1,61 @@
 // ==UserScript==
 // @name             STEAMNV
-// @namespace        Steam NSFW Verification
-// @description      Automate the verification of the Steam age verification, and/or comply with the NSFW warning webpage, for the Steam store and Community Hub.
+// @namespace        https://github.com/Beariddle/monkeyscript/
+// @description      Automatically verifies the Steam age verification, and/or complies with the NSFW warning webpage, for the Steam store and Community Hub.
 // @version          1.40
 // @author           Beariddle
 // @license          MIT; https://github.com/Beariddle/monkeyscript/blob/main/LICENSE.md
 // @supportURL       https://github.com/Beariddle/monkeyscript/issues
 // @downloadURL      https://raw.githubusercontent.com/Beariddle/monkeyscript/main/steamnv.user.js
 // @updateURL        https://raw.githubusercontent.com/Beariddle/monkeyscript/main/steamnv.user.js
+// @icon             https://store.steampowered.com/favicon.ico
 // @match            *://*.steampowered.com/agecheck/*
 // @match            *://steamcommunity.com/*
-// @run-at           document-start
 // ==/UserScript==
-const pathname = location.pathname;
-const hostname = location.hostname;
 
+/* Insert desired date of birth in-between the quotes. Leave the values in-between quotes empty, otherwise.
+ * Day format: Numbers below ten do not need a '0'.
+ * [Correct] 1, 2, 3, 4, ... || [incorrect] 01, 02, 03, ...
+ * Month format: must be written in full with a capital letter.
+ * E.g. January, February, March etc... */
+const Day		= '';
+const Month	= '';
+const Year	= '';
+
+// Define elements.
+const ageDay		= document.getElementById('ageDay');
+const ageMonth	= document.getElementById('ageMonth');
+const ageYear		= document.getElementById('ageYear');
 const agecheck_btn   = document.getElementsByClassName('btnv6_blue_hoverfade btn_medium')[0];
 const agegate_btn    = document.getElementById('age_gate_btn_continue');
 const partnerhub_btn = document.getElementsByClassName('partnereventshared_Button_1ABCO')[0];
 
+// Automatically generate a date of birth.
 function agecheck(months, rMonth) {
-  // Automatically generate a date of birth.
   // Generate the months.
   months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   rMonth = Math.floor(Math.random() * months.length);
 
   // Insert date values.
-  // Replace values with a static value when a specific date of birth is desired.
-  document.getElementById('ageDay').value = Math.floor((Math.random() * 28) + 1);
-  document.getElementById('ageMonth').value = months[rMonth];
-  document.getElementById('ageYear').value = Math.floor(Math.random() * (2000 - 1960)) + 1960;
+  ageDay.value		= Math.floor((Math.random() * 28) + 1);
+  ageMonth.value	= months[rMonth];
+  ageYear.value		= Math.floor(Math.random() * (2000 - 1960)) + 1960;
+}
 
-  // Automatically click the 'view page' button.
-  agecheck_btn.click();
-} // End of function agecheck
-
-if(hostname === 'store.steampowered.com') {
-  if(pathname.match('/agecheck/')) {
+// Insert values in the form and automatically click the 'view page' button on the corresponding NSFW warning subpages.
+if(location.hostname === 'store.steampowered.com' && location.pathname.match('/agecheck/')) {
+  if(Day <= null && Month <= null && Year <= null){
     agecheck();
+  } else if (Day !== null && Month !== null && Year !== null){
+    ageDay.value		= Day;
+    ageMonth.value	= Month;
+    ageYear.value		= Year;
   }
-} else if (hostname === 'steamcommunity.com') {
-  if(pathname.match('/app/') || pathname.match('/sharedfiles')) {
-    // Automatically click the 'view page' button on the NSFW warning webpages for the Steamcommunity hub.
+  agecheck_btn.click();
+} else if (location.hostname === 'steamcommunity.com') {
+  if(location.pathname.match('/app/') || location.pathname.match('/sharedfiles')) {
     agegate_btn.click();
-  } else if (pathname.match('/games/')) {
-    // Automatically click the 'view page' button on the NSFW warning webpage for pathname '/games/'.
+  } else if (location.pathname.match('/games/')) {
     partnerhub_btn.click();
   }
 }
